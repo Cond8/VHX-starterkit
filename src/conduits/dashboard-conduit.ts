1cd7697735eda800becdd70322cd0565f443cb9b
veighnsche <vincepaul.liem@gmail.com>
2025-04-27 21:39:36 +0200

    Initial commit: Set up VHX starter kit with core files and configurations

---
+++ src/conduits/dashboard-conduit.ts
@@ -0,0 +1,17 @@
+import { CoreRedprint, StrictObjectKVService } from '@cond8-ai/core';
+import { Context } from 'hono';
+import { createVHXActors } from '../actors/vhx';
+import { VHXService } from '../services/vhx-service';
+
+export class DashboardConduit extends CoreRedprint<Context> {
+	readonly locals = new StrictObjectKVService('locals');
+	readonly vhx = new VHXService('vhx');
+
+	constructor(c: Context) {
+		super(c);
+	}
+}
+
+export const DashboardActors = {
+	VHX: createVHXActors<DashboardConduit>(),
+};
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// src/index.ts
+import { Hono } from 'hono';
+import LandingPageDirector from './directors/landing-page-director';
+
+type Env = {};
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.get('/', async c => {
+	const html = await LandingPageDirector(c);
+	return c.html(html, 200, {
+		'Content-Type': 'text/html; charset=utf-8',
+	});
+});
+
+app.notFound(c =>
+	c.html('Not Found', 404, {
+		'Content-Type': 'text/html; charset=utf-8',
+	}),
+);
+
+export default app;
